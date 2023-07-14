621447c145911c0a58c2b7805e20fc34ebca3841
Roozbeh <roozbeh.ali.2005@gmail.com>
2023-07-14 00:09:05 -0400

    fixed about

--- pages/about.js
+++ pages/about.js
@@ -11,9 +11,9 @@ export default function Home() {
         <Text m={10} align='left'>Hi, I'm Roozbeh. I'm enrolled in Computer Engineering
           at the University of Waterloo, in my 1A term. Before that,
           I was a high schooler in the IB DP at Glenforest SS. I'm always
-          looking for new opportunities and people to meet. This website serves as a 
-          simple, unobtrusive walk through memory lane for me. You can reach me at&nbsp;
-          <Link color='gray' href="mailto:roozbeh.ali.2005@gmail.com">roozbeh.ali.2005@gmail.com</Link> 
+          looking for new opportunities and people to meet. This website showcases
+          my favorite projects and myself. You can reach me at&nbsp;
+          <Link color='gray' href="mailto:roozbeh.ali.2005@gmail.com">roozbeh.ali.2005@gmail.com</Link>
           &nbsp;for any inquiries.
         </Text>
       </Flex>
