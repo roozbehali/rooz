c3ea5e2f22e5ac452739b15a4a0823ce557198ab
Roozbeh <roozbeh.ali.2005@gmail.com>
2023-10-04 01:44:46 -0400

    'cleanup'

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-// const theme = extendTheme({
-//     fonts: {
-//       heading: `'Helvetica', sans-serif`,
-//       body: `'Raleway', sans-serif`,
-//     },
-// })
-
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
