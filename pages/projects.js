b8ddd75551e1a6bb83e890242c809e52635107fd
Roozbeh <roozbeh.ali.2005@gmail.com>
2023-11-30 00:53:32 -0500

    added tooltips on project headers

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import { Text, Grid, Center, Stack, Heading, Link } from '@chakra-ui/react'
+import { Text, Grid, Center, Stack, Heading, Link, Tooltip } from '@chakra-ui/react'
 import { motion } from "framer-motion"
 
 import { Navbar } from '@/components/navbar'
@@ -18,11 +18,12 @@ export default function Home() {
         templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
         gap={[10]}
         gridAutoFlow="dense">
-
           <Center rowSpan={1} colSpan={1} gap={5}>
             <img width='100px' height='100px' src='frc.png'/>
             <Stack spacing={1} direction='column' align='start'>
-              <Heading size='md'>FRC 6070</Heading>
+              <Tooltip label='Programming Captain / Driver' placement='top-start' bg='whiteAlpha' textColor='black'>
+                <Heading size='md'>FRC 6070</Heading>
+              </Tooltip>
               <Text>
                 2023 Season robot:&nbsp;
                 <Link as="a" target="_blank" href="https://frc6070.ca" aria-label="FRC" color='gray.500'>ICBM</Link>
@@ -33,27 +34,42 @@ export default function Home() {
           <Center rowSpan={1} colSpan={1} gap={5}>
             <img width='100px' height='100px' src='beavr.jpg'/>
             <Stack spacing={1} direction='column' align='start'>
+            <Tooltip label='Frontend developer' placement='top-start' bg='whiteAlpha' textColor='black'>
               <Heading size='md'>Beavr Labs</Heading>
+            </Tooltip>
               <Link as="a" target="_blank" href="https://1bitcoin.ca" aria-label="Beavr" color='gray.500'>1Bitcoin</Link>
             </Stack>
           </Center>
 
           <Center rowSpan={1} colSpan={1} gap={5}>
-            <img width='100px' height='100px' src='evlav.png'/>
+            <img width='100px' height='100px' src='uwaft.png'/>
             <Stack spacing={1} direction='column' align='start'>
-              <Heading size='md'>Hack3</Heading>
-              <Link as="a" target="_blank" href="https://devpost.com/software/evlav-detection" aria-label="Evlav" color='gray.500'>Evlav Detection</Link>
+            <Tooltip label='Firmware developer' placement='top-start' bg='whiteAlpha' textColor='black'>
+              <Heading size='md'>UWAFT</Heading>
+            </Tooltip>
+              <Link as="a" target="_blank" href="https://www.uwaft.ca/" aria-label="uwaft" color='gray.500'>Cadillac LYRIQ</Link>
             </Stack>
           </Center>
 
           <Center rowSpan={1} colSpan={1} gap={5}>
             <img width='100px' height='100px' src='markov.png'/>
             <Stack spacing={1} direction='column' align='start'>
+            <Tooltip label='Side project' placement='top-start' bg='whiteAlpha' textColor='black'>
               <Heading size='md'>IB Math IA</Heading>
+            </Tooltip>
               <Link as="a" target="_blank" href="https://drive.google.com/file/d/1IRmqeznFuIm5pbZcO5da306mM6C2B9rv/view" aria-label="IB" color='gray.500'>Monopoly with Markov Chains</Link>
             </Stack>
           </Center>
 
+          <Center rowSpan={1} colSpan={1} gap={5}>
+            <img width='100px' height='100px' src='evlav.png'/>
+            <Stack spacing={1} direction='column' align='start'>
+            <Tooltip label='Hackathon win' placement='top-start' bg='whiteAlpha' textColor='black'>
+              <Heading size='md'>Hack3</Heading>
+            </Tooltip>
+              <Link as="a" target="_blank" href="https://devpost.com/software/evlav-detection" aria-label="Evlav" color='gray.500'>Evlav Detection</Link>
+            </Stack>
+          </Center>
         </Grid>
         <Footer/>
       </center>
