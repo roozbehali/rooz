d5043fb4b6a213c4bc59a109a53bbebb3ffdda2a
Roozbeh <roozbeh.ali.2005@gmail.com>
2024-01-03 19:50:56 -0500

    about update

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ export default function Home() {
         <Navbar/>
         <Flex marginTop={[150, 200]} marginBottom={[94, 144]} w={[375, 400, 500, 600, 700]} bgColor='ghostwhite' borderRadius={10}>
           <Text m={[5, 10]} align='left'>Hi, I'm Roozbeh. I'm enrolled in Computer Engineering
-            at the University of Waterloo, in my 1A term. Before that,
+            at the University of Waterloo, in my 1B term. Before that,
             I was a high schooler in the IB DP at Glenforest SS. I'm always
             looking for new opportunities and people to meet. This website showcases
             my favorite projects and myself. You can reach me at&nbsp;
